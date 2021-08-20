cefbb614a627fe67164dbeee7544ac23135f0b67
ji-one <ds020228@naver.com>
2021-08-20 14:15:25 +0900

    build: db setting change sqlite to mysql

--- config/settings.py
+++ config/settings.py
@@ -119,15 +119,28 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.sqlite3', #Base-Dir : delivery-diary
+#         'NAME': BASE_DIR / 'db.sqlite3', #store data base-dir sqlite3
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.sqlite3', #Base-Dir : delivery-diary
-        'NAME': BASE_DIR / 'db.sqlite3', #store data base-dir sqlite3
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dd',
+        'USER': os.environ.get("MYSQL_USERNAME"),
+        'PASSWORD': os.environ.get("MYSQL_PASSWORD"),
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+        }
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
