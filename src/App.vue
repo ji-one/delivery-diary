1662d47a122b7aa53c1161f531d21e67ac30887a
ji-one <ds020228@naver.com>
2021-08-23 00:54:21 +0900

    style: update app-bar, delete navigation-drawer

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,89 @@
 <template>
   <v-app>
-    <v-app-bar app color="blue-grey lighten-2" clipped-left>
+    <v-app-bar app color="blue lighten-4">
+      <router-link to="/">
+        <v-btn icon>
+          <v-icon>mdi-home</v-icon>
+        </v-btn></router-link
+      >
       <v-spacer></v-spacer>
-      <router-link to="/"></router-link>
-      <v-btn v-if="!this.$store.state.login" text color="white" to="/signin"
+      <!-- TODO refactor: 버튼 속성 코드 중복 -->
+      <v-btn
+        v-if="!this.$store.state.login"
+        class="font-weight-bold"
+        color="blue-grey darken-1"
+        large
+        rounded
+        text
+        to="/signup"
+        >계정 만들기</v-btn
+      >
+      <v-btn
+        v-if="!this.$store.state.login"
+        class="font-weight-bold"
+        color="blue-grey darken-1"
+        large
+        rounded
+        text
+        to="/signin"
         >로그인</v-btn
       >
-      <v-btn v-if="!this.$store.state.login" text color="white" to="/signup"
-        >회원가입</v-btn
+      <v-btn
+        v-if="this.$store.state.login"
+        class="font-weight-bold"
+        color="blue-grey darken-1"
+        large
+        rounded
+        text
+        >배송 현황</v-btn
       >
-      <v-btn v-if="this.$store.state.login" text color="white" @click="signout"
-        >로그아웃</v-btn
+      <v-btn
+        v-if="this.$store.state.login"
+        class="font-weight-bold"
+        color="blue-grey darken-1"
+        large
+        rounded
+        text
+        >MY DASHBOARD</v-btn
       >
+      <v-menu offset-y v-if="this.$store.state.login">
+        <template v-slot:activator="{ on, attrs }">
+          <v-btn
+            class="font-weight-bold"
+            color="blue-grey darken-1"
+            large
+            rounded
+            text
+            v-bind="attrs"
+            v-on="on"
+          >
+            <profile />
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item to="/mypage">
+            <v-list-item-title>내 정보</v-list-item-title>
+          </v-list-item>
+          <v-list-item @click="signout">
+            <v-list-item-title>로그아웃</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
-    <v-navigation-drawer app clipped>
-      <v-list rounded>
-        <v-list-item to="/">
-          <v-list-item-icon>
-            <v-icon>mdi-home</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>Home</v-list-item-title>
-        </v-list-item>
-        <v-list-item v-if="this.$store.state.login" to="/mypage">
-          <v-list-item-icon>
-            <v-icon>mdi-account</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>My Page</v-list-item-title>
-        </v-list-item>
-        <v-list-item to="/dashboard">
-          <v-list-item-icon>
-            <v-icon>mdi-view-dashboard</v-icon>
-          </v-list-item-icon>
-          <v-list-item-title>Dashboard</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-main>
       <router-view />
     </v-main>
     <v-footer></v-footer>
   </v-app>
 </template>
-
 <script>
+import profile from "./components/App/Profile";
 export default {
   name: "App",
-
   data: () => ({}),
+  components: {
+    profile,
+  },
   methods: {
     signout() {
       this.$store.commit("del_token");
---
+++ src/components/App/Profile.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    {{ this.$store.state.nickname }}
+    <v-avatar size="38" class="ml-2 mr-2">
+      <v-img src="../../assets/profile.png"></v-img>
+    </v-avatar>
+    ▾
+  </div>
+</template>
