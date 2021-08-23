603a5b5cfdd30a891fde60babdc895026ba590c4
ji-one <ds020228@naver.com>
2021-08-23 15:44:17 +0900

    chore: add Contribution, WorldCloud component

---
+++ src/components/Dashboard/Contribution.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- <v-card-title>일기 로그</v-card-title> -->
+  <div id="cal-heatmap"></div>
+</template>
+<script>
+import CalHeatMap from "cal-heatmap";
+import "cal-heatmap/cal-heatmap.css";
+export default {
+  mounted() {
+    var cal = new CalHeatMap();
+    cal.init({});
+  },
+};
+</script>
